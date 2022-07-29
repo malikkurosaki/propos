b61abf55a860662a3d7a83ce86cf2bd34ce0405f
malikkurosaki <kurosakiblackangel@gmail.com>
2022-07-29 16:14:57 +0800

    Fri Jul 29 16:14:57 WITA 2022

--- server/bin/menus/git_push.js
+++ server/bin/menus/git_push.js
@@ -15,6 +15,4 @@ const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
 execSync(`git add . && git commit -m "$(date)" && git push origin ${branch}`, { stdio: 'inherit', cwd: path.join(__dirname, '../../../') });
 console.log(`push success branch : ${branch}`.cyan);
 
-
-
 // execSync(`git add . && git commit -m "auto commit && git push"`);
