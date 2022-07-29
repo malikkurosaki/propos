a19fb233c80d4e42eb88e3c5e8b3af4f27888d92
malikkurosaki <kurosakiblackangel@gmail.com>
2022-07-29 15:44:06 +0800

    auto commit

--- server/bin/menus/git_push.js
+++ server/bin/menus/git_push.js
@@ -16,7 +16,7 @@ const beautify = require('js-beautify').js_beautify;
     // find current branch
     const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
     execSync(`git add . && git commit -m "auto commit" && git push origin ${branch}`, { stdio: 'inherit' , cwd: path.join(__dirname, '../../../')});
-    console.log('push success'.cyan);
+    console.log(`push success branch : ${branch}`.cyan);
 })();
 
 
